da4f1f9760ad8dd47fc239c1e12770007fdadbff
abc <abc@gmail.com>
2026-06-10 10:23:53 +0000

    added c project

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Function declarations */
+void biggest3();
+void factorial();
+void pallindrom();
+
+int main()
+{
+    biggest3();
+    factorial();
+    pallindrom();
+
+   // return 0;
+}
